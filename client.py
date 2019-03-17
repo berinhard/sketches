bba8d28e17fa8419befefad07b03fb06086e2c6d
Bernardo Fontes <bernardoxhc@gmail.com>
2019-03-17 13:49:11 -0300

    Add command to create empty sketch

---
+++ client.py
@@ -0,0 +1,25 @@
+import click
+import os
+from cprint import cprint
+from shutil import copyfile
+from unipath import Path
+
+SKETCH_DIR = Path(__file__).parent
+TEMPLATES_DIR = SKETCH_DIR.child('templates')
+
+@click.command()
+@click.argument('dir_name')
+def configure_new_sketch(dir_name):
+    new_dir = SKETCH_DIR.child(dir_name)
+    new_sketch = new_dir.child(f'{dir_name}.pyde')
+    base_sketch = TEMPLATES_DIR.child('base_sketch.pyde')
+
+    if os.path.exists(new_dir):
+        cprint.err(f"There's already a sketch name {dir_name}", interrupt=True)
+
+    os.mkdir(new_dir)
+    copyfile(base_sketch, new_sketch)
+
+
+if __name__ == '__main__':
+    configure_new_sketch()
---
+++ templates/base_sketch.pyde
@@ -0,0 +1,15 @@
+# Author: Berin
+# Sketches repo: https://github.com/berinhard/sketches
+from berin.coors import draw_at_center, polar_coordinates
+from berin.grids import VirtualGrid
+from berin.palettes import get_color_palette
+from berin.save_frames import save_video_frames
+from berin.shapes import regular_polygon, draw_shape, lines_intersection, IntersectionLine
+
+
+def setup():
+    pass
+
+
+def draw():
+    pass
