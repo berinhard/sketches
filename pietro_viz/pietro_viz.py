f388aff6a5f951155d8acec22f001f39b387258d
Bernardo Fontes <bernardoxhc@gmail.com>
2020-07-13 21:06:26 -0300

    Add keyboard control to change patterns

--- pietro_viz/pietro_viz.py
+++ pietro_viz/pietro_viz.py
@@ -2,13 +2,29 @@
 # Sketches repo: https://github.com/berinhard/sketches
 
 from pattern_1 import Pattern1
+from pattern_2 import Pattern2
 
-pattern_1 = Pattern1()
+from random import choice
+
+patterns = {
+    '1': Pattern1(),
+    '2': Pattern2(),
+}
+
+active_pattern = choice(patterns.values())
 
 def setup():
     fullScreen()
-    pattern_1.prepare()
+    active_pattern.prepare()
 
 
 def draw():
-    pattern_1.draw_loop()
+    active_pattern.draw_loop()
+
+
+def keyPressed():
+    global active_pattern
+    pattern = patterns.get(key)
+    if pattern:
+        active_pattern = pattern
+        active_pattern.prepare()
