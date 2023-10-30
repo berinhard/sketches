45745f081ac88b8e111612d0fb1b91f5b650dac9
Bernardo Fontes <bernardoxhc@gmail.com>
2023-10-30 21:06:32 +0100

    Final proj

--- terra_estrago_ep/cover.py
+++ terra_estrago_ep/cover.py
@@ -1,6 +1,8 @@
+import itertools
 import random
 
 import py5
+import tqdm
 
 SIZE = 1200
 BACKGROUND = (242, 242, 242)
@@ -26,11 +28,10 @@ class Particle:
         self.x, self.y = x, y
         self.location = py5.Py5Vector(x, y)
         self.velocity = py5.Py5Vector(0, 0)
-        self.is_following = False
-        self.top_speed = py5.random(5, 10)
+        self.top_speed = py5.random(5, 7)
         self.previous = None
-        self.w = random.choice([0.5, 1, 1.5, 2, 2.5, 2.5, 2, 1.5, 1, 3])
-        self.b = int(py5.random(0, 60))
+        self.w = random.choice([1, 1, 2, 1.5, 2, 2.5, 2.5, 2, 1.5, 1, 3])
+        self.b = int(py5.random(10, 60))
         self.a = int(py5.random(160, 210))
 
     def move(self, target_pos=None):
@@ -38,18 +39,13 @@ class Particle:
         self.previous = py5.Py5Vector(x, y )
 
         acc_t = py5.Py5Vector(target_pos.x, target_pos.y)
-        acc_t.x =0
-        acc_t.y = 0
-        acc_t = TARGET
         acceleration = acc_t - self.location
-        self.is_following = True
 
         acc_multiplier = py5.Py5Vector.random(dim=2)
-        acc_multiplier *= py5.random(3)
+        acc_multiplier *= py5.random(4)
         acceleration += acc_multiplier
         acceleration.normalize()
-        acceleration *= 0.2
-
+        acceleration *= 0.1
 
         self.velocity += acceleration
         self.velocity.set_limit(self.top_speed)
@@ -68,15 +64,9 @@ class Particle:
 
 
 PARTICLES = []
-BOUNDARIES = []
 
 def populate_particles(n=1000, r=300):
     for i in range(n):
-        target = py5.Py5Vector(0, py5.random(py5.height / 2, py5.height))
-        if py5.random() > 0.5:
-            target = py5.Py5Vector(py5.random(0, py5.width / 2), py5.height)
-        BOUNDARIES.append(target)
-
         angle = py5.random(0, py5.TWO_PI)
         l = r + py5.random(-30, 30)
         coord = polar_coordinate(py5.width / 2, py5.height / 2, l, angle)
@@ -86,22 +76,22 @@ def populate_particles(n=1000, r=300):
 
 def setup():
     py5.size(SIZE, SIZE, py5.P2D)
-    py5.begin_record(py5.SVG, 'output.svg')
-    py5.background(*BACKGROUND)
+    #py5.begin_record(py5.SVG, 'output.svg')
+    py5.begin_record(py5.PDF, "vector.pdf");
+    #py5.background(*BACKGROUND)
+    points = list(itertools.product(range(SIZE), range(SIZE)))
+    for x, y in tqdm.tqdm(points):
+        c = [232, 220, 242]
+        random.shuffle(c)
+        py5.stroke(*c)
+        py5.point(x, y)
     populate_particles(1000)
 
 
 def draw():
+    target = py5.Py5Vector(py5.mouse_x, py5.mouse_y)
+    print(py5.frame_count, target)
     for i, particle in enumerate(PARTICLES):
-        target = BOUNDARIES[i]
-        offset = 1
-        if py5.random() >0.5:
-            if target.x == 0:
-                target.y -= offset
-            else:
-                target.x += offset
-
-        target = py5.Py5Vector(py5.width / 2, py5.height / 2)
         if particle.location.dist(target) < 10:
             target = py5.Py5Vector(particle.x, particle.y)
         particle.move(target)
@@ -112,8 +102,6 @@ def draw():
             py5.end_record()
             py5.save_frame(f"{py5.frame_count}.png")
             py5.no_loop()
-
-    print(py5.frame_count)
 
 
 def mouse_clicked(e):
