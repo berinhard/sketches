4afd27353978450e1a04c5329009d3829a973610
Bernardo Fontes <bernardoxhc@gmail.com>
2024-09-11 22:03:02 +0200

    Move sample code to its own sketch

--- be5/algorithms/wave_function_collapse.py
+++ be5/algorithms/wave_function_collapse.py
@@ -3,7 +3,6 @@ from collections import deque
 from dataclasses import dataclass, field
 from functools import cached_property
 from itertools import product
-from pathlib import Path
 
 import py5
 
@@ -186,51 +185,10 @@ class WaveFunctionCollapseGrid:
         """
         Gets the next pending cell with the lowes entropy and collapses it
         """
-        next_cell = sorted(grid.pending_cells, key=lambda c: c.entropy)[0]
-        grid.collapse_cell(next_cell)
+        next_cell = sorted(self.pending_cells, key=lambda c: c.entropy)[0]
+        self.collapse_cell(next_cell)
 
     def draw(self):
         for cell in self.cells:
             if cell.collapsed:
                 py5.image(cell.tile.image, cell.i * self.w, cell.j * self.h, self.w, self.h)
-            else:
-                py5.fill(0)
-                py5.stroke(100)
-                py5.rect(cell.i * self.w, cell.j * self.h, self.w, self.h)
-
-
-grid = None
-
-
-def setup():
-    global grid
-    py5.size(800, 800)
-
-    images_dir = Path.home() / "envs" / "sketches" / "be5" / "algorithms" / "tiles" / "demo"
-    blank = Tile.from_file(images_dir / "0.png", edges=[0, 0, 0, 0])
-    ptr_1 = Tile.from_file(images_dir / "1.png", edges=[0, 1, 1, 1])
-    ptr_2 = Tile.from_file(images_dir / "2.png", edges=[0, 1, 0, 1])
-
-    tiles = [
-        blank,
-        ptr_1,
-        ptr_1.rotate(1),
-        ptr_1.rotate(2),
-        ptr_1.rotate(3),
-        ptr_2,
-        ptr_2.rotate(1),
-    ]
-    grid = WaveFunctionCollapseGrid(dim=40, tiles=tiles)
-    grid.start()
-
-
-def draw():
-    grid.collapse()
-    grid.draw()
-
-    if grid.complete:
-        print("finished!")
-        py5.no_loop()
-
-
-py5.run_sketch()
